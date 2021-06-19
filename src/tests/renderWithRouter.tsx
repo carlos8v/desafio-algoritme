e9372fa034d8a4fc97741329b7d766f50178e98f
Carlos Souza <carlos.pessoal@hotmail.com>
2021-06-19 00:00:31 -0300

    refactor: changed the history format to help in tests

--- src/tests/renderWithRouter.tsx
+++ src/tests/renderWithRouter.tsx
@@ -3,8 +3,10 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-const renderWithRouter = (component: React.ReactElement) => {
-  const history = createMemoryHistory();
+const renderWithRouter = (
+  component: React.ReactElement,
+  history = createMemoryHistory()
+) => {
   return ({
     ...render(<Router history={history}>{component}</Router>), history,
   });
