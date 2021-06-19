481502c7d598b76b792d464567edd3e417020ed6
Carlos Souza <carlos.pessoal@hotmail.com>
2021-06-19 00:05:35 -0300

    feat: not found page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+    <section className="min-h-screen w-full bg-purple-800 flex items-center justify-center">
+      <h1 className="text-xl text-white font-bold">404 - Página não encontrada</h1>
+    </section>
+  );
+};
+
+export default NotFound;
---
+++ src/tests/NotFound.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { waitFor } from '@testing-library/react';
+import App from '../App';
+
+describe('Not found page', () => {
+  it('should not exist in the page', async () => {
+    const { queryByText } = renderWithRouter(<App />);
+    await waitFor(() => {
+      const element = queryByText(/404 - Página não encontrada/i);
+      expect(element).not.toBeInTheDocument();
+    });
+  });
+
+  it('should have the correct text', async () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    history.push('/not-exists');
+    await waitFor(() => {
+      const element = getByText(/404 - Página não encontrada/i);
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
